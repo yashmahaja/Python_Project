5e8a0d151b6bedbe4b11bba77d7d29195b65ed26
harsh21p <harshadpachore21@gmail.com>
2021-06-20 10:22:50 +0530

    myprofilepage

---
+++ database.sql
@@ -0,0 +1,4 @@
+create database data;
+use data;
+create table info1(name_user varchar(50),password_user varchar(50),email_user varchar(50),phone_user varchar(50),address_user varchar(50));
+select * from info1;
